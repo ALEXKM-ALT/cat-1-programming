b176bd0db5729ab81a71d061c664c9a46848256c
ALEXKM-ALT <mwangialexkinyua8@gmail.com>
2024-10-04 13:31:19 +0300

    Add files via upload

---
+++ cat 1 programing/loan.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int age, income;
+    char loanstatus
+    // input
+    printf("age");
+    scanf("%d",&age);
+    // check loan qualification
+    if (age>=21 &&income>=21000) {loan = 'Y';}
+    else {loan status ='N';}
+
+printf(conglatulations, you qualify to get a loan)
+return 0;
+}
